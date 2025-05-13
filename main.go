f582dbc696d8d4a59ae82436e5e40e1301a919bb
Thomas Runyan <runyant13@gmail.com>
2025-05-13 18:59:12 -0400

    Added new endpoint to check for server readiness

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+)
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/",  http.FileServer(http.Dir(".")))
+	mux.Handle("/app/",  http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
 
 	server := http.Server{
 		Handler: mux,
